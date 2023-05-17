3bc723c0cbb9718aeaaed0b7bcb4f876d28d53f8
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-05-17 23:49:38 +0200

    EVENTHANDLER: Added inventory check

--- Data Files/MWSE/mods/tew/Happenstance Hodokinesis/eventsHandler.lua
+++ Data Files/MWSE/mods/tew/Happenstance Hodokinesis/eventsHandler.lua
@@ -1,21 +1,26 @@
 local eventsHandler = {}
 
+local constants = require("tew.Happenstance Hodokinesis.constants")
 local controller = require("tew.Happenstance Hodokinesis.controller")
 
 function eventsHandler.onEquip(e)
 	if e.item then
 		if e.item.id then
-			if (e.reference == tes3.player) and (e.item.id == "tew_aleapsychon") then
+			if (e.reference == tes3.player) and (e.item.id == constants.ALEAPSYCHON_ID) then
 				controller.roll()
 			end
 		end
 	end
 end
 
--- TODO: Check if dice is in inventory
 function eventsHandler.onKeyDown(e)
 	if (e.isShiftDown) then
-		controller.roll()
+		for _, stack in pairs(tes3.player.object.inventory) do
+			if stack.object.id == constants.ALEAPSYCHON_ID then
+				controller.roll()
+				break
+			end
+		end
 	end
 end
 
